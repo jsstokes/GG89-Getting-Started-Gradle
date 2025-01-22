c99917a602ada177717894cd5d487395a9cb20bd
Scott Stokes <sstokes99@gmail.com>
2025-01-22 09:43:48 -0600

    Initial Changes - currently working with GG Enterprise Edition 8.9.15

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+import org.gradle.kotlin.dsl.repositories as repositories
+
+plugins {
+    id("application")
+}
+application {
+    mainClass = System.getProperty("mainClass")
+}
+
+group = "org.sstokes.demo"
+version = "1.0-SNAPSHOT"
+
+
+val gridgain_version = "8.9.15"
+
+
+repositories {
+    mavenCentral();
+    maven { url = uri("https://www.gridgainsystems.com/nexus/content/repositories/external") }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.gridgain:gridgain-core:$gridgain_version")
+    implementation("org.gridgain:ignite-spring:$gridgain_version")
+    implementation("org.gridgain:ignite-indexing:$gridgain_version")
+
+//    testImplementation('org.apache.ignite:ignite-core:${igniteVersion}')
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
